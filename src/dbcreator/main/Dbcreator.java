adf99a022c6d8660fb33b6fcef198f5a40598adf
Dario Bertini <berdario@gmail.com>
2010-03-15 15:33:18 +0100

    Refactored configuration out of Dbcreator

--- src/dbcreator/main/Dbcreator.java
+++ src/dbcreator/main/Dbcreator.java
@@ -8,158 +8,34 @@ import java.io.InputStreamReader;
 import java.lang.management.ManagementFactory;
 import java.util.logging.Logger;
 
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.PropertiesConfiguration;
 
 import dbcreator.scansione.BUnzip2;
 import dbcreator.scansione.ScanXML;
+import geotag.Configuration;
 
 public class Dbcreator {
-	private static String path,dbpath,cachepath,configfile,slash;
-	File requiredFiles[];
-	public int step=0;
+    Configuration config;
+    private static String path, dbpath, slash;
+    public int step = 0;
 	
     public Dbcreator(String configpath) {
-        
+
         //set default paths
-        String os = System.getProperty("os.name");
-        String homepath = System.getProperty("user.home");
-        slash = System.getProperty("file.separator");
-        if (os.startsWith("Linux")) {
-            path = System.getenv("XDG_DATA_HOME");
-            if (path == null) {
-                path = homepath + "/.local/share/";
-            }
-            path += "geosearch/";
-
-            configfile = System.getenv("XDG_CONFIG_HOME");
-            if (configfile == null) {
-                configfile = homepath + "/.config/";
-            }
-            configfile += "geosearch/config";
-
-            cachepath = System.getenv("XDG_CACHE_HOME");
-            if (cachepath == null) {
-                cachepath = homepath + "/.cache/";
-            }
-            cachepath += "geosearch/";
-
-        } else if (os.startsWith("Windows")) {
-            path = System.getenv("APPDATA") + slash + "geosearch" + slash;
-            configfile = path + "config";
-            cachepath = path + "cache" + slash;
-        } else if (os.startsWith("Mac")) {
-            path = homepath + "/Library/Application Support/geosearch/";
-            configfile = path + "config";
-            cachepath = path + "cache/";
-        } else {
-            path = System.getProperty("user.dir") + "geosearch" + slash;
-            configfile = path + "config";
-            cachepath = path + "cache" + slash;
-        }
-        dbpath = path + "db" + slash;
+        String paths[] = Configuration.getDefaultPaths();
         if (configpath != null) {
-            configfile = configpath;
+            paths[0] = configpath;
         }
 
-        loadConfiguration();
+        File cfgfile = new File(paths[0]);
+        if (!cfgfile.exists()) {
+            Configuration.createDefaultConfiguration(paths[0], paths[1], paths[3]);
+        }
+        config = new Configuration(paths[0]);
+
+        path = Configuration.getPath();
+        dbpath = Configuration.getDbPath();
+        slash = Configuration.getSeparator();
     }
-
-	public void loadConfiguration(){//TODO: originalmente dbcreator e geoapplication erano in 2 progetti separati, ma ora che sono nello stesso sarebbe conveniente unire questa funzione in una sola
-		File cfgfile=new File(configfile);
-		if (!cfgfile.exists()){
-			createDefaultConfiguration();
-		}
-		try {
-			PropertiesConfiguration config = new PropertiesConfiguration(configfile);
-			
-			path=config.getString("basepath");
-			dbpath=path+config.getString("dbdirectory")+slash;
-			
-			String[] requiredFileNames=config.getStringArray("requiredfiles");
-			String[] requiredDbFileNames=config.getStringArray("requireddbfiles");
-			File[] innerRequiredFiles=new File[requiredDbFileNames.length+requiredFileNames.length];
-			
-			int i=0;
-			for (String rf : requiredFileNames){
-				innerRequiredFiles[i]=new File(path+rf);
-				i++;
-			}
-			
-			for (String rf : requiredDbFileNames){
-				innerRequiredFiles[i]=new File(dbpath+rf);
-				i++;
-			}
-			requiredFiles=innerRequiredFiles;
-			
-		} catch (ConfigurationException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private void createDefaultConfiguration() {
-		String input=null,cacheinput="";
-		
-		File cfgfile=new File(configfile);
-		File cfgpath=cfgfile.getParentFile();
-		if (!cfgpath.exists()){
-			cfgpath.mkdirs();
-		}
-		
-		String filenames[]={"datiscritti.dat","datiscritti.idx","albero_Btree_osm.db",
-				"albero_Btree_population.db","albero_Btree_population2.db",
-				"albero_Btree_Gazetteer.db", "albero_Btree_Intermedio.db",
-				"albero_alternatenames.db","albero_alternatenamesId.db",
-				"albero_admin1codeascii.db","albero_countryInfo.db",
-				"albero_featurecodes.db"};
-		try{//fare attenzione: tutte le funzioni interne si aspettano path, assoluti... TODO sanitarizzare
-			System.out.println("Missing configuration file, do you want to create one? [Y/n]");
-			BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
-			while (true){
-				input = in.readLine();
-				if (input.equalsIgnoreCase("y")||input.equals("")){
-					System.out.println("Please insert data path ["+path+"]:");
-					input=in.readLine();
-					if (!input.equals("")){
-						cachepath=input+"cache/";
-						System.out.println("Please insert cache path ["+cachepath+"]:");
-						cacheinput=in.readLine();
-					}
-					break;
-				}else if (input.equalsIgnoreCase("n")){
-					System.exit(0);
-				}
-			}
-			
-			cfgfile.createNewFile();
-			
-			PropertiesConfiguration config=new PropertiesConfiguration(configfile);
-			if (input.equals("")){
-				config.setProperty("basepath", path);
-				config.setProperty("cachepath", cachepath);
-			} else {
-				config.setProperty("basepath", input);
-				if (cacheinput.equals("")){
-					config.setProperty("cachepath", cachepath);
-				} else {
-					config.setProperty("cachepath", cacheinput);
-				}
-			}
-			config.setProperty("dbdirectory", "db");
-			config.setProperty("requiredfiles", "coordinate.txt");
-			config.setProperty("requireddbfiles", filenames);
-			config.setProperty("languagefile", "englishSW.txt");
-			
-			config.save();
-		} catch (ConfigurationException e) {		
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
 
     public boolean checkExistingDb() {
         boolean flag = true;
@@ -167,12 +43,12 @@ public class Dbcreator {
         File basepath = new File(path);
         if (!basepath.exists()) {
             basepath.mkdirs();
-            System.out.println("Per favore inserisci i file necessari in " + path);
+            checkNeededFiles();
             System.exit(1);
         } else {
             System.out.println("Verifica presenza strutture dati:");
             try {
-                for (File f : requiredFiles) {
+                for (File f : Configuration.getRequiredFiles()) {
                     System.out.println(f.getCanonicalPath() + ": " + (f.exists() ? "OK" : "MANCA"));
                     if (!f.exists()) {
                         flag = false;
--- src/geotag/Configuration.java
+++ src/geotag/Configuration.java
@@ -5,7 +5,10 @@
 
 package geotag;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
@@ -21,37 +24,22 @@ public class Configuration {
     private static String cachepath = null;
     private static String language = null;
 
-    PropertiesConfiguration config;
     private ArrayList<File> indexDirs;
+
+    static File requiredFiles[];
+
+    PropertiesConfiguration config;
 
     public Configuration(String configfile) {
         slash = System.getProperty("file.separator");
 
-        if (configfile == null){
+        if (configfile == null) {
             // set default config path
-
-            String os = System.getProperty("os.name");
-            String homepath = System.getProperty("user.home");
-            if (os.startsWith("Linux")) {
-
-                configfile = System.getenv("XDG_CONFIG_HOME");
-                if (configfile == null) {
-                    configfile = homepath + "/.config/";
-                }
-                configfile += "geosearch/config";
-
-            } else if (os.startsWith("Windows")) {
-                configfile = System.getenv("APPDATA") + slash + "geosearch" + slash + "config";
-            } else if (os.startsWith("Mac")) {
-                configfile = homepath + "/Library/Application Support/geosearch/" + "config";
-            } else {
-                configfile = System.getProperty("user.dir") + "geosearch" + slash + "config";
-            }
+            configfile = getDefaultPaths()[0];
         }
 
         File cfgfile = new File(configfile);
         if (!cfgfile.exists()) {
-            System.out.println("Missing configuration file, please run Dbcreator first");
             throw new ConfigFileNotFoundException();
         }
         
@@ -61,9 +49,136 @@ public class Configuration {
             dbpath = path + config.getString("dbdirectory") + slash;
             cachepath = config.getString("cachepath");
             language = path + "stopWords" + slash + config.getString("languagefile");
+
+            String[] requiredFileNames = config.getStringArray("requiredfiles");
+            String[] requiredDbFileNames = config.getStringArray("requireddbfiles");
+            File[] innerRequiredFiles = new File[requiredDbFileNames.length + requiredFileNames.length];
+
+            int i = 0;
+            for (String rf : requiredFileNames) {
+                innerRequiredFiles[i] = new File(path + rf);
+                i++;
+            }
+
+            for (String rf : requiredDbFileNames) {
+                innerRequiredFiles[i] = new File(dbpath + rf);
+                i++;
+            }
+            requiredFiles = innerRequiredFiles;
+
         } catch (ConfigurationException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void createDefaultConfiguration(String configfile, String path, String cachepath) {
+        String input = null, inputCachepath = "";
+
+        File cfgfile = new File(configfile);
+        File cfgpath = cfgfile.getParentFile();
+        if (!cfgpath.exists()) {
+            cfgpath.mkdirs();
+        }
+
+        String filenames[] = {"datiscritti.dat", "datiscritti.idx", "albero_Btree_osm.db",
+            "albero_Btree_population.db", "albero_Btree_population2.db",
+            "albero_Btree_Gazetteer.db", "albero_Btree_Intermedio.db",
+            "albero_alternatenames.db", "albero_alternatenamesId.db",
+            "albero_admin1codeascii.db", "albero_countryInfo.db",
+            "albero_featurecodes.db"};
+        try {//fare attenzione: tutte le funzioni interne si aspettano path, assoluti... TODO sanitarizzare
+            System.out.println("Missing configuration file, do you want to create one? [Y/n]");
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                input = in.readLine();
+                if (input.equalsIgnoreCase("y") || input.equals("")) {
+                    System.out.println("Please insert data path [" + path + "]:");
+                    input = in.readLine();
+                    if (!input.equals("")) {
+                        cachepath = input + "cache/";
+                        System.out.println("Please insert cache path [" + cachepath + "]:");
+                        inputCachepath = in.readLine();
+                    }
+                    break;
+                } else if (input.equalsIgnoreCase("n")) {
+                    System.exit(0);
+                }
+            }
+
+            cfgfile.createNewFile();
+
+            PropertiesConfiguration config = new PropertiesConfiguration(cfgfile);
+            if (input.equals("")) {
+                config.setProperty("basepath", path);
+                config.setProperty("cachepath", cachepath);
+            } else {
+                config.setProperty("basepath", input);
+                if (inputCachepath.equals("")) {
+                    config.setProperty("cachepath", cachepath);
+                } else {
+                    config.setProperty("cachepath", inputCachepath);
+                }
+            }
+            config.setProperty("dbdirectory", "db");
+            config.setProperty("requiredfiles", "coordinate.txt");
+            config.setProperty("requireddbfiles", filenames);
+            config.setProperty("languagefile", "englishSW.txt");
+
+            config.save();
+        } catch (ConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * @return Array with 0: config file path, 1: default base path, 2: default db path,
+     * 3: default cache path
+     */
+    public static String[] getDefaultPaths(){
+        String configfile;
+        String os = System.getProperty("os.name");
+        String homepath = System.getProperty("user.home");
+        slash = System.getProperty("file.separator");
+        if (os.startsWith("Linux")) {
+            path = System.getenv("XDG_DATA_HOME");
+            if (path == null) {
+                path = homepath + "/.local/share/";
+            }
+            path += "geosearch/";
+
+            configfile = System.getenv("XDG_CONFIG_HOME");
+            if (configfile == null) {
+                configfile = homepath + "/.config/";
+            }
+            configfile += "geosearch/config";
+
+            cachepath = System.getenv("XDG_CACHE_HOME");
+            if (cachepath == null) {
+                cachepath = homepath + "/.cache/";
+            }
+            cachepath += "geosearch/";
+
+        } else if (os.startsWith("Windows")) {
+            path = System.getenv("APPDATA") + slash + "geosearch" + slash;
+            configfile = path + "config";
+            cachepath = path + "cache" + slash;
+        } else if (os.startsWith("Mac")) {
+            path = homepath + "/Library/Application Support/geosearch/";
+            configfile = path + "config";
+            cachepath = path + "cache/";
+        } else {
+            path = System.getProperty("user.dir") + "geosearch" + slash;
+            configfile = path + "config";
+            cachepath = path + "cache" + slash;
+        }
+        dbpath = path + "db" + slash;
+        
+        String[] result = {configfile, path, dbpath, cachepath};
+        
+        return result;
     }
 
     public ArrayList<File> updateIndexConfig(String inputpath) {
@@ -115,6 +230,10 @@ public class Configuration {
 
     public static String getSWLanguage(){
         return language;
+    }
+
+    public static File[] getRequiredFiles(){
+        return requiredFiles;
     }
 
     class ConfigFileNotFoundException extends RuntimeException{
